6f529ab0f2ee7518176049f421e3f8a23352e02f
Viet Anh <vietanh2732004@gmail.com>
2025-02-24 02:47:27 +0700

    Add Firebase Login via Email & Google

--- SafeguardSystem.BLL/obj/Debug/net8.0/SafeguardSystem.BLL.AssemblyInfo.cs
+++ SafeguardSystem.BLL/obj/Debug/net8.0/SafeguardSystem.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeguardSystem.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a279663da4e8d2a9a0539c4f70e28121df43bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb093e9e1a9390fad082dfe1c5b61073c33d090")]
 [assembly: System.Reflection.AssemblyProductAttribute("SafeguardSystem.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeguardSystem.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
